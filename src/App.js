15548fd3aae76d99502847e1f6aa44d4343e9663
Taejung Kim <hi2177@naver.com>
2022-05-04 17:47:30 +0900

    feat: Header 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import Header from "./components/Header.js";
 
 export default function App({ $target }) {
+  const header = new Header({
+    $target,
+  });
 }
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+export default function Header({ $target }) {
+  const $header = document.createElement("div");
+  $header.classList = "header"
+  $target.appendChild($header);
+
+  $header.innerHTML = `
+  
+    <div class="header__circle"></div>
+    <h4 class="header__title">
+    te-ing’s Portfolio
+    </h4>
+    <div class="header__circle"></div>
+  `;
+
+}
